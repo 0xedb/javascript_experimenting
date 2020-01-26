fbe212848006136caa1248cd7c90bdcdb8f721f1
theBashShell <edohbruno@gmail.com>
2020-01-26 23:58:03 +0000

    weakmap example

--- weakmap.js
+++ weakmap.js
@@ -14,11 +14,11 @@ mp.set(two, 2);
 weakMp.set(one, 1);
 weakMp.set(two, 2);
 
-console.log("before delete", mp);
-console.log("before delete", weakMp);
+console.log("before delete", mp.get(one));
+console.log("before delete", weakMp.get(one));
 
 one = null;
 two = null;
 
-console.log("after delete", mp);
-console.log("after delete", weakMp);
+console.log("after delete", mp.get(one));
+console.log("after delete", weakMp.get(one));
